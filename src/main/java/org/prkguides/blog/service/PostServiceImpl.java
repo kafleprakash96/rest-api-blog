e730d9d908084108af1859bf175b6e432f2e1a66
Prakash Kafle <kafleprakash96@gmail.com>
2023-12-31 02:00:36 -0600

    Refactored DTO/Entity conversion with ModelMapper

--- src/main/java/org/prkguides/blog/service/PostServiceImpl.java
+++ src/main/java/org/prkguides/blog/service/PostServiceImpl.java
@@ -1,12 +1,17 @@
 package org.prkguides.blog.service;
 
+import org.modelmapper.ModelMapper;
 import org.prkguides.blog.dto.PostDto;
 import org.prkguides.blog.entity.Post;
 import org.prkguides.blog.repository.PostRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostServiceImpl implements PostService {
+
+    @Autowired
+    private ModelMapper modelMapper;
 
     private PostRepository postRepository;
 
@@ -17,18 +22,12 @@ public class PostServiceImpl implements PostService {
     public PostDto createPost(PostDto postDto) {
 
         //Convert Dto to entity
-        Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setContent(postDto.getContent());
-        post.setDescription(postDto.getDescription());
+        Post post = modelMapper.map(postDto,Post.class);
 
         Post newPost = postRepository.save(post);
 
         //Convert entity to DTO
-        PostDto postResponse = new PostDto();
-        postResponse.setId(newPost.getId());
-        postResponse.setContent(newPost.getContent());
-        postResponse.setDescription(postDto.getDescription());
+        PostDto postResponse = modelMapper.map(newPost,PostDto.class);
 
         return postResponse;
     }
